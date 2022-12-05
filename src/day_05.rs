4c3fb1177cfc63c79c039b3f911f21187c1a06a1
BartTerpstra <bartterpstra@protonmail.com>
2022-12-05 21:02:46 +0100

    day 5 part 2

--- src/day_05.rs
+++ src/day_05.rs
@@ -56,7 +56,7 @@ pub fn part1(input: &Input) -> Output {
 
         instructions.push((amount, from, to));
     }
-    println!("instructions: \n{:?}", instructions);
+    // println!("instructions: \n{:?}", instructions);
 
     for instruction in instructions {
         //illegal because it's still unstable
@@ -68,16 +68,73 @@ pub fn part1(input: &Input) -> Output {
             grid[(instruction.2 - 1) as usize].push(item_to_move);
         }
     }
-    println!("final state: {:?}", grid);
+    // println!("final state: {:?}", grid);
 
-    for x in grid {
-        print!("{}", x.last().unwrap());
-    }
-    println!();
+    let answer: String = grid.iter().fold(String::new(), |mut x, y| {
+        x.push(*y.last().unwrap());
+        return x;
+    });
 
-    Output::U32(0)
+    Output::String(answer)
 }
 
 pub fn part2(input: &Input) -> Output {
-    Output::U32(0)
+    //create array of lists of items representing the dock
+    let mut grid: ArrayVec<Vec<char>, 9> = ArrayVec::new();
+    for x in 0..9 {
+        grid.push(Vec::new());
+    }
+
+    // for each line, check every container space on that height. save it if there is something
+    for x in input.lines().take(8) {
+        //IF grid
+        let charline: Vec<char> = x.chars().collect();
+        for x in 0..9 {
+            let char = charline[x * 4 + 1];
+            if char != ' ' {
+                grid[x].push(char);
+            }
+        }
+    }
+
+    grid.iter_mut().for_each(|c| c.reverse());
+
+    // println!("grid: \n {:?}", grid);
+
+    let mut instructions: Vec<(u8, u8, u8)> = Vec::new();
+
+    for instruction in input.lines().skip(10) {
+        if instruction.is_empty() {
+            continue;
+        }
+
+        let tokens: Vec<&str> = instruction.split(' ').collect();
+        let amount: u8 = tokens[1].parse::<u8>().unwrap();
+        let from: u8 = tokens[3].parse::<u8>().unwrap();
+        let to: u8 = tokens[5].parse::<u8>().unwrap();
+
+        instructions.push((amount, from, to));
+    }
+    // println!("instructions: \n{:?}", instructions);
+
+    for instruction in instructions {
+        //illegal because it's still unstable
+        //let items_to_move = &grid[instruction.1 as usize].take(instruction.0..);
+
+        let mut items_to_move: Vec<char> = Vec::new();
+        for countdown in 0..instruction.0 {
+            items_to_move.push(grid[(instruction.1 - 1) as usize].pop().unwrap());
+        }
+        for countdown in 0..instruction.0 {
+            grid[(instruction.2 - 1) as usize].push(items_to_move.pop().unwrap());
+        }
+    }
+    // println!("final state: {:?}", grid);
+
+    let answer: String = grid.iter().fold(String::new(), |mut x, y| {
+        x.push(*y.last().unwrap());
+        return x;
+    });
+
+    Output::String(answer)
 }
