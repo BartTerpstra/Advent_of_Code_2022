81c0d1d8ac6de0c2d29a3cbb650559c4c154eb3b
BartTerpstra <bartterpstra@protonmail.com>
2022-12-10 23:26:01 +0100

    day 10 part 1

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,6 +1,10 @@
 use crate::day_10::InstructionType::{ADDX, NOP};
 use crate::{Output, Part};
 use arrayvec::ArrayVec;
+use derive_more::Display;
+use itertools::Itertools;
+use regex::internal::Inst;
+use std::ops::Add;
 
 const INPUT: &str = include_str!("../input/10.txt");
 
@@ -11,14 +15,15 @@ struct Instruction {
     value: i32,
 }
 
+#[derive(Display)]
 enum InstructionType {
     ADDX,
     NOP,
 }
 pub fn read() -> Input {
-    //TODO basically just string slice INPUT by line and select and convert to correct type.
     INPUT
         .split('\n')
+        .filter(|x| !x.is_empty())
         .map(|x| {
             let arr: Vec<&str> = x.split(' ').take(2).collect();
             let instruction = match arr[0] {
@@ -47,8 +52,49 @@ pub fn run(part: Part) -> Output {
     }
 }
 
+//too high 33600
+//incorrect -19220
+//incorrect -14800
 pub fn part1(input: &Input) -> Output {
-    Output::U32(0)
+    let mut answers: Vec<i32> = Vec::new();
+    let mut register = 1;
+    let mut pending = -1;
+
+    let pol_cycles: Vec<u8> = vec![20, 60, 100, 140, 180, 220];
+    let mut input = input.iter();
+    let mut occupied: u32 = 0;
+
+    for x in 1..u8::MAX {
+        //evaluate register
+        if pol_cycles.contains(&x) {
+            println!("register {}", register);
+            println!("x {}", x);
+            answers.push(x as i32 * register)
+        }
+
+        if occupied == 0 {
+            let potential_instruction = input.next();
+            if potential_instruction.is_none() {
+                break;
+            }
+            let instruction = potential_instruction.unwrap();
+
+            match instruction.instruction_type {
+                ADDX => {
+                    pending = instruction.value;
+                    occupied += 1;
+                }
+                NOP => {}
+            }
+        } else {
+            occupied -= 1;
+            if occupied == 0 {
+                register += pending;
+            }
+        }
+    }
+
+    Output::I32(answers.iter().sum())
 }
 
 pub fn part2(input: &Input) -> Output {
