e716b78d93cec727f628692dbd6dd46c41a18307
BartTerpstra <bartterpstra@protonmail.com>
2022-12-11 11:42:22 +0100

    day 11 part 1 broken

--- src/day_11.rs
+++ src/day_11.rs
@@ -1,21 +1,73 @@
 use crate::{Output, Part};
 use arrayvec::ArrayVec;
+use itertools::{Itertools, PeekingNext};
+use std::usize;
 
 const INPUT: &str = include_str!("../input/11_test.txt");
 
-pub type Input = Vec<str>;
+pub type Input = Vec<Monkey>;
 
-// todo struct monkey
-//has: Vec<Items>, some kind of function, divistibility, left partner, right partner
 struct Monkey {
-    items: Vec<Items>,
-    empathy: dyn Fn(usize) -> usize,
+    items: Vec<u8>,
+    empathy: Box<dyn Fn(usize) -> usize>,
     divisibility: u32,
     left_partner: usize,
     right_partner: usize,
 }
+
 pub fn read() -> Input {
-    INPUT.lines().collect()
+    let mut monkeys = Vec::new();
+    INPUT.split("\n\n").for_each(|x| {
+        let lines: Vec<&str> = x.lines().collect();
+        let items: Vec<u8> = lines[1]
+            .strip_prefix("  Starting items: ")
+            .unwrap()
+            .split(", ")
+            .map(|x| x.parse::<u8>().unwrap())
+            .collect();
+
+        let components: Vec<&str> = lines[2]
+            .strip_prefix("  Operation: new = old ")
+            .unwrap()
+            .split(" ")
+            .collect();
+        assert_eq!(components.len(), 2, "operation string parse error");
+        let operator = components[0].chars().next().unwrap();
+        let value = components[1].parse::<usize>().unwrap();
+
+        let empathy = match operator {
+            '+' => Box::new(|x: usize| x + value) as Box<dyn Fn(usize) -> usize>,
+            '*' => Box::new(|x: usize| x * value) as Box<dyn Fn(usize) -> usize>,
+            _ => panic!("expression parse failed"),
+        };
+
+        let divisibility: u32 = lines[3]
+            .strip_prefix("  Test: divisible by ")
+            .unwrap()
+            .parse()
+            .unwrap();
+        let left_partner: usize = lines[4]
+            .strip_prefix("    If true: throw to monkey ")
+            .unwrap()
+            .parse()
+            .unwrap();
+        let right_partner: usize = lines[5]
+            .strip_prefix("    If false: throw to monkey ")
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        let new_monkey = Monkey {
+            items,
+            empathy,
+            divisibility,
+            left_partner,
+            right_partner,
+        };
+        monkeys.push(new_monkey);
+    });
+
+    monkeys
 }
 
 pub fn run(part: Part) -> Output {
