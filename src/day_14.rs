2a012630171887d12489678e5d9bcd22aba024b1
BartTerpstra <bartterpstra@protonmail.com>
2022-12-15 12:09:57 +0100

    setup for 13-15

---
+++ src/day_14.rs
@@ -0,0 +1,27 @@
+use crate::{Output, Part};
+use arrayvec::ArrayVec;
+
+const INPUT: &str = include_str!("../input/14_test.txt");
+
+pub type Input = ArrayVec<u8, 1024>; //todo example, do change
+
+pub fn read() -> Input {
+    //TODO basically just string slice INPUT by line and select and convert to correct type.
+    INPUT
+}
+
+pub fn run(part: Part) -> Output {
+    let input = read();
+    match part {
+        Part::One => part1(&input),
+        Part::Two => part2(&input),
+    }
+}
+
+pub fn part1(input: &Input) -> Output {
+    Output::U32(0)
+}
+
+pub fn part2(input: &Input) -> Output {
+    Output::U32(0)
+}
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ mod day_09;
 mod day_10;
 mod day_11;
 mod day_12;
+mod day_13;
+mod day_14;
+mod day_15;
 mod helper;
 
 use helper::*;
